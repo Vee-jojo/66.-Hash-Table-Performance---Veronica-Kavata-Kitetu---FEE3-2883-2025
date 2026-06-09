208b81956f63dd75cd7478e1b32f00b534158500
Vee-jojo <fee328832025@students.uonbi.ac.ke>
2026-06-09 19:24:47 +0300

    Implementation Hash Chaining
    Implementation/HashChaining.src.cpp

---
+++ HashChaining.src.cpp
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<CodeBlocks_project_file>
+	<FileVersion major="1" minor="6" />
+	<Project>
+		<Option title="Implementation src/HashChaining.cpp" />
+		<Option execution_dir="C:/Users/PC/Desktop/New folder/COMP CODE/HashChaining.cpp/Implementation src/" />
+		<Option pch_mode="2" />
+		<Option compiler="gcc" />
+		<Build>
+			<Target title="Debug">
+				<Option output="bin/Debug/Implementation src/HashChaining.cpp" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Debug/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-g" />
+				</Compiler>
+			</Target>
+			<Target title="Release">
+				<Option output="bin/Release/Implementation src/HashChaining.cpp" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Release/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-O2" />
+				</Compiler>
+				<Linker>
+					<Add option="-s" />
+				</Linker>
+			</Target>
+		</Build>
+		<Compiler>
+			<Add option="-Wall" />
+			<Add option="-fexceptions" />
+		</Compiler>
+		<Unit filename="../New folder/COMP CODE/HashChaining.cpp/Implementation src/main.cpp" />
+		<Extensions />
+	</Project>
+</CodeBlocks_project_file>
